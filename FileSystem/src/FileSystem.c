a5fa4615f1da331f1903be4efae55a0f943b3730
macetosella <macetosella@est.utn.frba.edu.ar>
2019-03-30 16:01:01 -0300

    arreglo problema de commons

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -10,8 +10,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "config_fileSystem.h"
 
 int main(void) {
+	get_parametros_config();
+	configure_logger();
 	puts("Hello FileSystem!!"); /* prints  */
 	return EXIT_SUCCESS;
 }
