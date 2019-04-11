737b24cb7917b5dcf1fa84dfbe84097dec7b3669
Matias Tomasone <matias.tomasone@gmail.com>
2019-04-11 10:31:51 -0300

    Parser guarda en array las consultas

--- Kernel/src/kernel.c
+++ Kernel/src/kernel.c
@@ -27,45 +27,84 @@ int main(void) {
 void leer_consola(){
 	char* leido;
 
-	puts("");
 	leido = readline(">>");
+
+	string_to_upper(leido);
 
 		while (strncmp(leido, "EXIT", 4) != 0){
 
 			//loguear valor leido
 			parser_lql(leido);
-			//puts(leido);
 			free(leido);
 			leido = readline(">>");
+			string_to_upper(leido);
 		}
 
-	puts(leido);
 	free(leido);
 	puts("Salgo de la consola");
 
 }
 
 void parser_lql(char * consulta){
+	//falla cuando escribis una consulta sin todas sus partes, porque hace un puts de un elemento del array que no existe, verificar eso.
 
-	string_to_upper(consulta);
+	char** consulta_parseada = string_split(consulta, " "); //agregar timestamp al final
 
-	if (string_starts_with(consulta, "SELECT")){
+	if (string_starts_with(consulta_parseada[0], "SELECT")){
+
 		puts("Es un SELECT");
+		puts(consulta_parseada[0]);//select
+		puts(consulta_parseada[1]);//tabla
+		puts(consulta_parseada[2]);//key
 	}
-	else if (string_starts_with(consulta, "INSERT")){
+	else if (string_starts_with(consulta_parseada[0], "INSERT")){
+
 		puts("Es un INSERT");
+		puts(consulta_parseada[0]);//insert
+		puts(consulta_parseada[1]);//tabla
+		puts(consulta_parseada[2]);//key
+		puts(consulta_parseada[3]);//valor
+		puts(consulta_parseada[4]);//timestamp
 	}
-	else if (string_starts_with(consulta, "CREATE")){
+	else if (string_starts_with(consulta_parseada[0], "CREATE")){
+
 		puts("Es un CREATE");
+		puts(consulta_parseada[0]);//CREATE
+		puts(consulta_parseada[1]);//tabla
+		puts(consulta_parseada[2]);//consistencia
+		puts(consulta_parseada[3]);//particiones
+		puts(consulta_parseada[4]);//compactacion
+		puts(consulta_parseada[5]);//timestamp
+
 	}
-	else if (string_starts_with(consulta, "DESCRIBE")){
+	else if (string_starts_with(consulta_parseada[0], "DESCRIBE")){
+
 		puts("Es un DESCRIBE");
+		puts(consulta_parseada[0]);//DESCRIBE
+		puts(consulta_parseada[1]);//tabla
+		puts(consulta_parseada[2]);//timestamp
 	}
-	else if (string_starts_with(consulta, "DROP")){
+	else if (string_starts_with(consulta_parseada[0], "DROP")){
+
 		puts("Es un DROP");
+		puts(consulta_parseada[0]);//DROP
+		puts(consulta_parseada[1]);//tabla
+		puts(consulta_parseada[2]);//timestamp
 	}
-	else if (string_starts_with(consulta, "ADD MEMORY")){
+	else if (string_starts_with(consulta_parseada[0], "ADD MEMORY")){
+
 		puts("Es un ADD MEMORY");
+		puts(consulta_parseada[0]);//ADD
+		puts(consulta_parseada[1]);//MEMORY
+		puts(consulta_parseada[2]);//memoria
+		puts(consulta_parseada[3]);//timestamp
+	}
+	else if (string_starts_with(consulta_parseada[0], "RUN")){
+
+		puts("Es un RUN");
+		puts(consulta_parseada[0]);//RUN
+		puts(consulta_parseada[1]);//archivo
+		puts(consulta_parseada[2]);//timestamp
 	}
 	else {
 		puts("ERROR: La consulta debe comenzar con alguno de [SELECT, INSERT, CREATE, DESCRIBE, DROP, ADD MEMORY]");
