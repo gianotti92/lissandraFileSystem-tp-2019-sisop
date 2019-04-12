301a94c5f54d2506708aa02b80a15ab4c59062c6
Matias Tomasone <matias.tomasone@gmail.com>
2019-04-11 21:02:06 -0300

    Las consultas ahora tienen timestamp y los valores que tienen que ser numericos son validados

--- Kernel/src/kernel.c
+++ Kernel/src/kernel.c
@@ -60,79 +60,131 @@ void parser_lql(char * consulta){
 
 	char** consulta_parseada = string_split(consulta, " ");
 
+	int length = cantidad_elementos(consulta_parseada) - 1;
+
 	if (string_starts_with(consulta_parseada[0], "SELECT")){
 
-		//int cant_elem_consulta = cantidad_elementos(consulta_parseada); //no funciona aun
-		//puts(cant_elem_consulta);
+		if (length != 3){
+			puts("ERROR: La sintaxis correcta es > SELECT [NOMBRE_TABLA] [KEY]");
+		}
+		else if(!es_numero(consulta_parseada[2])){
+			puts("ERROR: La Key debe ser un numero.");
+		}
+		else{
+			puts(consulta_parseada[0]);//select
+			puts(consulta_parseada[1]);//tabla
+			puts(consulta_parseada[2]);//key
+			puts(consulta_parseada[3]);//timestamp
 
-		puts(consulta_parseada[0]);//select
-		puts(consulta_parseada[1]);//tabla
-		puts(consulta_parseada[2]);//key
-		puts(consulta_parseada[3]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "INSERT")){
 
-		puts("Es un INSERT");
-		puts(consulta_parseada[0]);//insert
-		puts(consulta_parseada[1]);//tabla
-		puts(consulta_parseada[2]);//key
-		puts(consulta_parseada[3]);//valor
-		puts(consulta_parseada[4]);//timestamp
+		if (length != 4){
+			puts("ERROR: La sintaxis correcta es > INSERT [NOMBRE_TABLA] [KEY] \“[VALUE]\”");
+		}
+		else if (!es_numero(consulta_parseada[2])){
+			puts("ERROR: La Key debe ser un numero.");
+		}
+		else{
+			puts(consulta_parseada[0]);//insert
+			puts(consulta_parseada[1]);//tabla
+			puts(consulta_parseada[2]);//key
+			puts(consulta_parseada[3]);//valor
+			puts(consulta_parseada[4]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "CREATE")){
-
-		puts("Es un CREATE");
-		puts(consulta_parseada[0]);//CREATE
-		puts(consulta_parseada[1]);//tabla
-		puts(consulta_parseada[2]);//consistencia
-		puts(consulta_parseada[3]);//particiones
-		puts(consulta_parseada[4]);//compactacion
-		puts(consulta_parseada[5]);//timestamp
-
+		if (length != 5){
+			puts("ERROR: La sintaxis correcta es > CREATE [NOMBRE_TABLA] [TIPO_CONSISTENCIA] [NUMERO_PARTICIONES] [COMPACTION_TIME]");
+		}
+		else if (!es_numero(consulta_parseada[3])){
+			puts("ERROR: La cantidad de particiones debe ser un numero.");
+		}
+		else if (!es_numero(consulta_parseada[4])){
+			puts("ERROR: El tiempo de compactacion debe ser un numero.");
+		}
+		else{
+			puts(consulta_parseada[0]);//CREATE
+			puts(consulta_parseada[1]);//tabla
+			puts(consulta_parseada[2]);//consistencia
+			puts(consulta_parseada[3]);//particiones
+			puts(consulta_parseada[4]);//compactacion
+			puts(consulta_parseada[5]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "DESCRIBE")){
 
-		puts("Es un DESCRIBE");
-		puts(consulta_parseada[0]);//DESCRIBE
-		puts(consulta_parseada[1]);//tabla
-		puts(consulta_parseada[2]);//timestamp
+		if (length != 2){
+			puts("ERROR: La sintaxis correcta es > DESCRIBE [NOMBRE_TABLA]");
+		}
+		else{
+			puts(consulta_parseada[0]);//DESCRIBE
+			puts(consulta_parseada[1]);//tabla
+			puts(consulta_parseada[2]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "DROP")){
 
-		puts("Es un DROP");
-		puts(consulta_parseada[0]);//DROP
-		puts(consulta_parseada[1]);//tabla
-		puts(consulta_parseada[2]);//timestamp
+		if (length != 2){
+			puts("ERROR: La sintaxis correcta es > DROP [NOMBRE_TABLA]");
+		}
+		else{
+			puts(consulta_parseada[0]);//DROP
+			puts(consulta_parseada[1]);//tabla
+			puts(consulta_parseada[2]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "ADD MEMORY")){
 
-		puts("Es un ADD MEMORY");
-		puts(consulta_parseada[0]);//ADD
-		puts(consulta_parseada[1]);//MEMORY
-		puts(consulta_parseada[2]);//memoria
-		puts(consulta_parseada[3]);//timestamp
+		if (length != 5){
+			puts("ERROR: La sintaxis correcta es > ADD MEMORY [NÚMERO] TO [CRITERIO]");
+		}
+		else{
+			puts(consulta_parseada[0]);//ADD
+			puts(consulta_parseada[1]);//MEMORY
+			puts(consulta_parseada[2]);//memoria
+			puts(consulta_parseada[3]);//TO
+			puts(consulta_parseada[4]);//criterio
+			puts(consulta_parseada[5]);//timestamp
+		}
 	}
 	else if (string_starts_with(consulta_parseada[0], "RUN")){
 
-		puts("Es un RUN");
-		puts(consulta_parseada[0]);//RUN
-		puts(consulta_parseada[1]);//archivo
-		puts(consulta_parseada[2]);//timestamp
+		if (length != 2){
+			puts("ERROR: La sintaxis correcta es > RUN [ARCHIVO]");
+		}
+		else{
+			puts(consulta_parseada[0]);//RUN
+			puts(consulta_parseada[1]);//archivo
+			puts(consulta_parseada[2]);//timestamp
+		}
 	}
 	else {
-		puts("ERROR: La consulta debe comenzar con alguno de [SELECT, INSERT, CREATE, DESCRIBE, DROP, ADD MEMORY]");
+		puts("ERROR: Las consultas disponibles son [SELECT, INSERT, CREATE, DESCRIBE, DROP, ADD MEMORY]");
 	}
 }
 
 
 int cantidad_elementos(char ** array){
 	int i = 0;
-	puts("entre");
 
-	while (array[i] != NULL){
+	while (array[i] != '\0'){
 		i++;
 	}
 
 	return i;
 
+}
+
+int es_numero(char* palabra){
+	int length = string_length(palabra);
+
+	for (int i=0; i<length; i++){
+		if(!isdigit(palabra[i])){
+			return 0;
+		}
+	}
+
+	return 1;
 }
