ed88b4fecb6b7c27253df594f1debe2d89799f31
Christian Contarino <ccontarino@gmail.com>
2019-05-02 04:01:44 -0300

    refactor fs

--- FileSystem/src/createFile.c
+++ FileSystem/src/createFile.c
@@ -7,8 +7,12 @@
 #include <commons/string.h>
 #include <utilguenguencha/comunicacion.h>
 #include <utilguenguencha/utils.h>
-int main() {
+#include <unistd.h>
+#include <ftw.h>
 
+
+int main() {
+	configuracion_inicial_file_system();
 	Create * createTable = malloc(sizeof(Create));
 
 	MockTabla(&createTable);
@@ -98,4 +102,67 @@ void escribirMetadata(char * metaDataPath, Create * createTable) {
 	free(formato);
 
 }
+void configuracion_inicial_file_system(){
 
+		t_config* CONFIG;
+		CONFIG = config_create("Metadata/metadata.bin");
+		if (!CONFIG) {
+			printf("No encuentro el archivo config\n");
+			exit_gracefully(EXIT_FAILURE);
+		}
+		BLOCK_SIZE = config_get_int_value(CONFIG,"BLOCK_SIZE");
+		BLOCKS = config_get_int_value(CONFIG,"BLOCKS");
+	//	MAGIC_NUMBER = string_new();
+		crear_bloques(BLOCKS);
+	//	string_append(&MAGIC_NUMBER, config_get_int_value(CONFIG,"MAGIC_NUMBER"));
+		config_destroy(CONFIG);
+
+}
+
+void crear_bloques(int cantidad_bloques){
+	crear_folder_bloques();
+	char * pathTable = string_new();
+		string_append(&pathTable, "FS_LISSANDRA/Bloques");
+		for (int i = 0; i < cantidad_bloques; ++i) {
+			char * numberBin = string_new();
+			string_append(&numberBin, pathTable);
+			string_append(&numberBin, "/");
+			string_append(&numberBin, string_itoa(i));
+			string_append(&numberBin, ".bin");
+			int filedescriptor = open(numberBin, O_RDWR | O_APPEND | O_CREAT, 0777);
+			if (filedescriptor < 0) {
+				perror("Error creating my_log file\n");
+				exit(-1);
+			}
+			free(numberBin);
+		}
+		free(pathTable);
+
+}
+void crear_folder_bloques() {
+	//mkdir("FS_LISSANDRA");
+	mkdir("FS_LISSANDRA", 0777);
+	mkdir("FS_LISSANDRA/Bloques", 0777);
+}
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+	{
+	    int rv = remove(fpath);
+
+	    if (rv)
+	        perror(fpath);
+
+	    return rv;
+	}
+
+
+}
+int rmrf(char *path)
+	{
+	    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+	}
+int dropTable()
+	{
+		char *path = string_new();
+		string_append(&path, "tables/name");
+rmrf(path);
+	}
