ec981a87bf3aaf165feec25caf9bc177ff2fa271
Matias Tomasone <matias.tomasone@gmail.com>
2019-06-11 21:39:01 -0300

    Agergo semaforos.h en comunicacion

--- utilguenguencha/src/comunicacion.h
+++ utilguenguencha/src/comunicacion.h
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <error.h>
 #include "utils.h"
+#include <semaphore.h>
 
 #define BACKLOG 10     // Cuántas conexiones pendientes se mantienen en cola
 extern int errno;
