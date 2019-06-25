cc52ba12d75e123231c44648c7d3d15fa6d30339
matiasBusco <matias.busco@mercadolibre.com>
2019-06-25 14:52:20 -0300

    duplicate all generico

--- utilguenguencha/src/utils.c
+++ utilguenguencha/src/utils.c
@@ -132,8 +132,9 @@ Retorno_Describe *duplicar_describe(Retorno_Describe *describe){
 
 t_list * list_duplicate_all(t_list *lista, void*(*duplicador)(void*), pthread_mutex_t mutex){
 	t_list *duplicate = list_create();
-	void duplicar(Memoria *memoria){
-		list_add(duplicate, duplicador(memoria));
+	
+	void duplicar(void *element){
+		list_add(duplicate, duplicador(element));
 	}
 	pthread_mutex_lock(&mutex);
 	list_iterate(lista, (void*)duplicar);
